1e1872ba5039bfbc4f2f20ea836ff17e047bd19e
Neil Matatall <oreoshake@users.noreply.github.com>
2017-06-23 13:07:16 -1000

    actually add the structured scope file

---
+++ lib/hackerone/client/structured_scope.rb
@@ -0,0 +1,29 @@
+module HackerOne
+  module Client
+    class StructuredScope
+      DELEGATES = [
+        :asset_identifier,
+        :asset_type,
+        :eligible_for_bounty,
+        :eligible_for_submission,
+        :instruction
+      ]
+
+      delegate *DELEGATES, to: :attributes
+
+      def initialize(scope)
+        @scope = scope
+      end
+
+      def id
+        @scope[:id]
+      end
+
+      private
+
+      def attributes
+        OpenStruct.new(@scope[:attributes])
+      end
+    end
+  end
+end

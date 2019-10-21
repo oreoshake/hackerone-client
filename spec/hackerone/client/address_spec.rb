be23349e4071931a0e1e2b12dda738698eecbc43
Brian Anglin <anglinb@github.com>
2019-10-21 15:03:38 -0700

    Adds tests for swag and address

---
+++ spec/hackerone/client/address_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+require 'active_support/core_ext/hash'
+
+RSpec.describe HackerOne::Client::Address do
+  let (:address_data) do
+     {
+      :name=>"Brian Anglin",
+      :street=>" 88 Colin P Kelly Jr Street",
+      :city=>"San Francisco",
+      :postal_code=>"94107",
+      :state=>"California",
+      :country=>"United States",
+      :created_at=>"2019-10-21T20:50:40.159Z",
+      :tshirt_size=>"M_Medium",
+      :phone_number=>"555-555-5555"
+    }
+  end
+
+  let(:example) do
+    {
+      :id=>"7374",
+      :type=>"address",
+      :attributes=> address_data
+    }.with_indifferent_access
+  end
+
+  it 'creates the address type with attributes' do
+    address = HackerOne::Client::Address.new example
+    expect(address.class).to eq described_class
+    expect(address.id).to eq "7374"
+
+    address_data.keys.each do |key|
+      expect(address.send key.to_s).to eq address_data[key]
+    end
+  end
+end
---
+++ spec/hackerone/client/swag_spec.rb
@@ -0,0 +1,72 @@
+require "spec_helper"
+
+RSpec.describe HackerOne::Client::Swag do
+  let(:api) { HackerOne::Client::Api.new("github") }
+
+  before(:all) do
+    ENV["HACKERONE_TOKEN_NAME"] = "foo"
+    ENV["HACKERONE_TOKEN"] = "bar"
+  end
+
+  let(:program) do
+    VCR.use_cassette(:programs) do
+      HackerOne::Client::Program.find "github"
+    end
+  end
+
+  let(:swags) do
+    VCR.use_cassette(:swag) do
+      program.swag
+    end
+  end
+
+  let (:swag){ swags[0] }
+
+  it "returns a collection" do
+    expect(swags).to be_kind_of(Array)
+    expect(swags.size).to eq(8)
+  end
+
+  it "returns id" do
+    expect(swag.id).to be_present
+    expect(swag.id).to eq('3377')
+  end
+
+  it "returns sent?" do
+    expect(swag.sent?).to be(false)
+  end
+
+
+  describe "address" do
+
+    it "returns an address if present" do
+      address = swag.address
+      expect(address).to be_present
+      expect(address).to be_kind_of(HackerOne::Client::Address)
+    end
+
+    it "returns nil if not present" do
+      address = swags[1].address
+      expect(address).to be_nil
+    end
+  end
+
+
+  describe "user" do
+    it "returns a user" do
+      user = swag.user
+      expect(user).to be_present
+      expect(user).to be_kind_of(HackerOne::Client::User)
+    end
+  end
+
+  describe "mark_as_sent!" do
+    it "should mark the swag as sent" do
+      VCR.use_cassette(:swag_sent) do
+        result = swag.mark_as_sent!
+        expect(result).to be_present
+        expect(result.sent?).to be true
+      end
+    end
+  end
+end
